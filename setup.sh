4301c849119c4e3c2067f65ace415f011901aff5
parbol <pablom@cern.ch>
2025-03-12 09:48:53 +0000

    Modified setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-echo "Setting up environment in Galois"
-export G4INSTALLDIR=/home/pablo/geant4-v11.1.2-install/
-export G4WORKDIR=/home/pablo/Documentos/PROTECT/PROTECTSimulator/
-export JSONCPPDIR=/home/pablo/jsoncpp/
-source $G4INSTALLDIR/bin/geant4.sh
-export PYTHONPATH=$G4WORKDIR/dataAnalysis/
-source /home/pablo/root_v6.28.04-install/bin/thisroot.sh
+if [ $HOSTNAME == "galois" ]; then
+	echo "Setting up environment in Galois"
+	export G4INSTALLDIR=/home/pablo/geant4-v11.1.2-install/
+	export G4WORKDIR=/home/pablo/Documentos/PROTECT/PROTECTSimulator/
+	export JSONCPPDIR=/home/pablo/jsoncpp/
+	source $G4INSTALLDIR/bin/geant4.sh
+	export PYTHONPATH=$G4WORKDIR/dataAnalysis/
+	source /home/pablo/root_v6.28.04-install/bin/thisroot.sh
+fi
+
+if [ $HOSTNAME == "deep-gan" ]; then
+        echo "Setting up environment in deep gan"
+        export G4INSTALLDIR=/home/leire/geant4-v11.1.2-install/
+        export G4WORKDIR=/home/leire/PROTECT/PROTECTSimulator/
+        export JSONCPPDIR=/home/leire/jsoncpp/
+        source $G4INSTALLDIR/bin/geant4.sh
+        export PYTHONPATH=$G4WORKDIR/dataAnalysis/
+        source /home/leire/root_v6.28.04-install/bin/thisroot.sh
+fi
 
 
