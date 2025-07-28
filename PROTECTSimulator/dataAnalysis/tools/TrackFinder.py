758fb9893da2fa1dcc271b7cf35aecc0f34a48fc
parbol <pablom@cern.ch>
2025-07-28 08:56:34 +0200

    New Rotating stuff

--- PROTECTSimulator/dataAnalysis/tools/TrackFinder.py
+++ PROTECTSimulator/dataAnalysis/tools/TrackFinder.py
@@ -27,22 +27,44 @@ class TrackFinder:
             if len(l) == 0:
                 layer1Complete = False
         
+
         if layer1Complete:
             tracks1 = self.runDetector(layers1, ev)
         tracks2 = self.runDetector(layers2, ev)
-       
-        self.track1 = Track() 
-        for layer in bestTrack1:
-            self.track1.insertHit(ev.x[layer], ev.y[layer], ev.z[layer], ev.toa[layer], ev.genEnergy[layer])
-        self.track2 = Track() 
-        for layer in bestTrack2:
-            self.track2.insertHit(ev.x[layer], ev.y[layer], ev.z[layer], ev.toa[layer], ev.genEnergy[layer])
-        self.track1.build()
-        self.track2.build()
+        
+        self.makeAssociation(tracks1, tracks2)
+        return tracks1, tracks2
 
+
+    def makeAssociation(self, tracks1, tracks2):
+
+        thresholdScore = 1.0
+        for i, tr in enumerate(tracks2):
+            tr.associator = i
+        if len(tracks1) != 0:
+            for i, tr2 in enumerate(tracks2):
+                for j, tr1 in enumerate(tracks1):
+                    if i == j:
+                        continue
+                    score = self.compatibility(tr1, tr2)
+                    if score > thresholdScore:
+                        continue
+                     
+
+    def addHitToList(self, listOfHits, track):
+
+        newhits = []
+        for hit in track.hits:
+            if hit not in listOfHits:
+                newhits.append(hit)
+            else:
+                return False
+        listOfHits.append(newhits)
+        return True
     
     def runDetector(self, layers, ev):
         
+        tracks = []
         #First we make all the possible tracks with chi2 smaller than a given threshold
         chi2threshold = 1.0
         theElement = (0)
@@ -55,9 +77,14 @@ class TrackFinder:
             if t.chi2 < chi2threshold:
                 tracks.append(t)
         #We sort the list by chi2
+        sortedTracks = sorted(tracks, key=lambda track: track.chi2)
+        #Finally we remove tracks that are using hits that have been already used
         listOfHits = []
-         
-        return theElement
+        finalTracks = []
+        for i in range(0, len(sortedTracks)): 
+            if self.addHitToList(listOfHits, sortedTracks[i]):
+                finalTracks.append(sortedTracks[i])
+        return finalTracks
 
 
         
